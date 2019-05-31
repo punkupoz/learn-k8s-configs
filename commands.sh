e0977f907955ef717398d4f194bab6dd56e448e5
punkupoz <punkupoz@gmail.com>
2019-05-31 15:54:06 +1000

    asd

--- commands.sh
+++ commands.sh
@@ -8,7 +8,11 @@ kubectl get -n kube-system deployments
 
 kubectl get -n kube-system pods
 
+kubectl get -n kube-system pods
+
 kubectl get -n kube-system svc
+
+kubectl get -n kube-system ds
 
 kubectl get -n kube-system configmaps
 
@@ -21,15 +25,30 @@ kubectl logs -n kube-system -f
 kubectl describe -n kube-system configmaps ingress-controller-leader-nginx
 kubectl describe -n kube-system configmaps nginx-load-balancer-conf
 
+kubectl patch svc my-nginx-nginx-ingress-controller -n kube-system -p '{"spec": {"type": "LoadBalancer", "externalIPs":["10.192.0.2"]}}'
+kubectl patch svc -p '{"spec": {"type": "LoadBalancer", "externalIPs":["10.192.0.2"]}}'
+
 kubectl config use-context minikube
 kubectl config use-context dind
 
+kubectl describe -n kube-system deployments my-nginx-nginx-ingress-controller
+
+helm delete --purge my-nginx
 
 helm install stable/nginx-ingress \
-  --namespace=kube-system \
   --name=my-nginx \
-  --set controller.service.nodePorts.http='30001' \
-  --set controller.service.nodePorts.https='30002' \
-  --set defaultBackend.service.type='NodePort'
+  --set controller.hostNetwork='true' \
+  --set controller.service.externalIPs[0]='10.192.0.2'
+
+
+
+  --set defaultBackend.service.type='NodePort' \
+  --set controller.nodeSelector."kubernetes\.io/hostname"="kube-node-2"
+  --set controller.service.loadBalancerIP='10.192.0.2' \
+  --namespace=kube-system \
+  
+  -f helm/nginx.yaml
 
   http://teriterimasumasu.kawaii
+
+  http://teriterimasumasu.kawaii:30001
